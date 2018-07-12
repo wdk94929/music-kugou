1c9b2f5d05e399e6147eb7c0eca45b34cd8543ae
862079044@qq.com <862079044@qq.com>
2018-07-12 11:25:59 +0800

    merge

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import elementUI from 'element-ui'
+
+
 import App from './App.vue'
+
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.prototype.$axios = axios
+Vue.use(elementUI)
+
 
 new Vue({
   el: '#app',
